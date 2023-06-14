75029d3ac252e9bb431f62f20a30ad5a4c2190cf
yuni-hutsuka <yuni.wille999@gmail.com>
2023-06-14 19:10:30 +0900

    feat: ファイル入力を可能にした
    とりあえずRustJP

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,19 @@
 use std::env;
+use std::fs::File;
+use std::io::prelude::*;
 
 fn main() {
     println!("Welcome to My Stack-VM.");
 
     let args: Vec<String> = env::args().collect();
-    println!("{:?}", args);
+    let filename = &args[1];
+
+    println!("In the {}", filename);
+
+    let mut f = File::open(filename).expect("file not found");
+    let mut contents = String::new();
+    f.read_to_string(&mut contents)
+        .expect("Something went wrong reading the file contents");
+
+    println!("With text:\n{}", contents);
 }
