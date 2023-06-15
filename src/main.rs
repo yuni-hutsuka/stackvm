13c1719df3a57900a84f72d28b81f1597fac1bbb
yuni-hutsuka <yuni.wille999@gmail.com>
2023-06-16 07:17:24 +0900

    feat: implement program_counter

--- src/main.rs
+++ src/main.rs
@@ -2,59 +2,47 @@ use std::env;
 use std::fs::File;
 use std::io::prelude::*;
 
-struct Stack {
-    stack: Vec<i32>,
-}
+// struct Stack {
+//     stack: Vec<i32>,
+// }
 
-impl Stack {
-    fn new() -> Self {
-        Stack { stack: Vec::new() }
-    }
-    fn push(&mut self, value: i32) {
-        self.stack.push(value)
-    }
-    fn pop(&mut self) -> Option<i32> {
-        self.stack.pop()
-    }
-    fn is_empty(&self) -> bool {
-        self.stack.is_empty()
-    }
-    fn length(&mut self) -> usize {
-        self.stack.len()
-    }
-}
+// impl Stack {
+//     fn new() -> Self {
+//         Stack { stack: Vec::new() }
+//     }
+//     fn push(&mut self, value: i32) {
+//         self.stack.push(value)
+//     }
+//     fn pop(&mut self) -> Option<i32> {
+//         self.stack.pop()
+//     }
+//     fn is_empty(&self) -> bool {
+//         self.stack.is_empty()
+//     }
+//     fn length(&mut self) -> usize {
+//         self.stack.len()
+//     }
+// }
 
 fn main() {
-    println!("Welcome to My Stack-VM.");
-
+    // 引数受け取り
     let args: Vec<String> = env::args().collect();
+
+    // ファイルオープン
     let filename = &args[1];
-
-    println!("target name is: {}", filename);
-
     let mut f = File::open(filename).expect("file not found");
     let mut contents = String::new();
     f.read_to_string(&mut contents)
         .expect("Something went wrong reading the file contents");
 
-    println!("With text:\n```\n{}\n```", contents);
+    // contentsを分割
+    let splitted = contents.lines().collect::<Vec<&str>>();
+    let mut program_counter = Vec::new();
 
-    let mut stack: Stack = Stack::new();
+    for line in splitted {
+        let tmp = line.split_whitespace().collect::<Vec<&str>>();
+        program_counter.push(tmp);
+    }
 
-    stack.push(10);
-    println!("I pushed 10");
-    let tmp: i32 = stack.pop().unwrap();
-    println!("I poped and result is {}", tmp);
-    println!("I think stack is empty now. -> {:?}", stack.is_empty());
-    stack.push(10);
-    println!("I pushed 10");
-    stack.push(-10);
-    println!("I pushed -10");
-    println!("I think stack length is 2 now. -> {:?}", stack.length());
-    let tmp: i32 = stack.pop().unwrap();
-    println!("I poped and result is {}", tmp);
-    let tmp: i32 = stack.pop().unwrap();
-    println!("I poped and result is {}", tmp);
-    println!("I think stack is empty now. -> {:?}", stack.is_empty());
-    println!("I think next action is caused panic! -> {}", stack.pop().unwrap());
+    // let mut stack: Stack = Stack::new();
 }
