c6f0741bb4ad40eb3d5a90ec7dfda7463f224950
yuni-hutsuka <yuni.wille999@gmail.com>
2023-06-16 08:10:57 +0900

    feat: add 4 arithmetic operations
    四則演算を追加した。
    雑実装最高

--- src/main.rs
+++ src/main.rs
@@ -2,27 +2,27 @@ use std::env;
 use std::fs::File;
 use std::io::prelude::*;
 
-// struct Stack {
-//     stack: Vec<i32>,
-// }
+struct Stack {
+    stack: Vec<i32>,
+}
 
-// impl Stack {
-//     fn new() -> Self {
-//         Stack { stack: Vec::new() }
-//     }
-//     fn push(&mut self, value: i32) {
-//         self.stack.push(value)
-//     }
-//     fn pop(&mut self) -> Option<i32> {
-//         self.stack.pop()
-//     }
-//     fn is_empty(&self) -> bool {
-//         self.stack.is_empty()
-//     }
-//     fn length(&mut self) -> usize {
-//         self.stack.len()
-//     }
-// }
+impl Stack {
+    fn new() -> Self {
+        Stack { stack: Vec::new() }
+    }
+    fn push(&mut self, value: i32) {
+        self.stack.push(value)
+    }
+    fn pop(&mut self) -> Option<i32> {
+        self.stack.pop()
+    }
+    fn is_empty(&self) -> bool {
+        self.stack.is_empty()
+    }
+    fn length(&mut self) -> usize {
+        self.stack.len()
+    }
+}
 
 fn main() {
     // 引数受け取り
@@ -44,5 +44,61 @@ fn main() {
         program_counter.push(tmp);
     }
 
-    // let mut stack: Stack = Stack::new();
+    let mut stack: Stack = Stack::new();
+
+    for i in 0..program_counter.len() {
+        let tmp = &program_counter[i];
+        let cmd = tmp[0].to_string();
+        match &*cmd {
+            "push" => {
+                println!("push! -> {}", tmp[1]);
+                stack.push(tmp[1].parse::<i32>().unwrap());
+            },
+            "pop" => {
+                if stack.is_empty(){
+                    println!("Stack is empty!");
+                    continue;
+                }
+                let result = stack.pop();
+                println!("popped! -> {}", result.unwrap());
+            },
+            "add" => {
+                if stack.length() < 2 {
+                    println!("lower!");
+                    continue;
+                }
+                let result = stack.pop().unwrap() + stack.pop().unwrap();
+                println!("added! -> {}", result);
+                stack.push(result);
+            },
+            "sub" => {
+                if stack.length() < 2 {
+                    println!("lower!");
+                    continue;
+                }
+                let result = stack.pop().unwrap() - stack.pop().unwrap();
+                println!("subtracted! -> {}", result);
+                stack.push(result);
+            },
+            "mul" => {
+                if stack.length() < 2 {
+                    println!("lower!");
+                    continue;
+                }
+                let result = stack.pop().unwrap() * stack.pop().unwrap();
+                println!("multipled! -> {}", result);
+                stack.push(result);
+            },
+            "div" => {
+                if stack.length() < 2 {
+                    println!("lower!");
+                    continue;
+                }
+                let result = stack.pop().unwrap() / stack.pop().unwrap();
+                println!("divided! -> {}", result);
+                stack.push(result);
+            },
+            _ => (),
+        }
+    }
 }
